7ae7e65dd196b602f3ef3a847b15c41274e956d9
M. Juaristi <monikajuaristi@gmail.com>
2016-05-02 19:27:34 +0200

    menu modified

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
 
-    //Animation mobile menu
     $( ".btn-anim-three" ).on( "click", function() {
       $(this).toggleClass( "closed" );
       $('.menu-mobile').toggleClass( "closed" );
     });
 
-    //Animation content
     $('.js-animate').addClass("hidden").viewportChecker({
         classToAdd: 'visible animated fadeInDown',
         offset: 100
